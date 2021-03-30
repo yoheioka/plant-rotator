9e44d427f1ed7cc6d3d533c44987d6c47152c426
yoheioka <yoyo716jp@gmail.com>
2021-03-30 14:36:41 -0400

    add set angle function

--- servomotor.py
+++ servomotor.py
@@ -8,23 +8,36 @@ GPIO.setup(servoPIN, GPIO.OUT)
 p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
 p.start(2.5) # Initialization
 try:
-  while True:
-    p.ChangeDutyCycle(5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(10)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(12.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(10)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(2.5)
-    time.sleep(0.5)
+    while True:
+        # p.ChangeDutyCycle(5)
+        # time.sleep(0.5)
+        # p.ChangeDutyCycle(7.5)
+        # time.sleep(0.5)
+        # p.ChangeDutyCycle(10)
+        # time.sleep(0.5)
+        # p.ChangeDutyCycle(12.5)
+        # time.sleep(0.5)
+        # p.ChangeDutyCycle(10)
+        # time.sleep(0.5)
+        # p.ChangeDutyCycle(7.5)
+        # time.sleep(0.5)
+        # p.ChangeDutyCycle(5)
+        # time.sleep(0.5)
+        # p.ChangeDutyCycle(2.5)
+        # time.sleep(0.5)
+        setAngle(90)
+        time.sleep(0)
+        setAngle(90)
+        time.sleep(0)
 except KeyboardInterrupt:
   p.stop()
   GPIO.cleanup()
+
+
+def set_angle(angle):
+    duty = angle / 18 + 2
+    GPIO.output(03, True)
+    pwm.ChangeDutyCycle(duty)
+    sleep(1)
+    GPIO.output(03, False)
+    pwm.ChangeDutyCycle(0)
