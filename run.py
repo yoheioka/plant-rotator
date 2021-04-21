2652ca6f2a887e3b7cdf9d4fc580e74f00b5fb7b
Yohei Oka <yoheioka@yoheis-mbp.myfiosgateway.com>
2021-04-21 14:18:25 -0400

    fix run.py

--- run.py
+++ run.py
@@ -1,4 +1,3 @@
-import seeed_si114x
 import RPi.GPIO as GPIO
 import time
 from influxdb import InfluxDBClient
@@ -9,42 +8,47 @@ ROTATE_THRESH = 50000
 
 
 def rotate():
-    print('start rotate')
-    try:
-        GPIO.setmode(GPIO.BOARD)
-        control_pins = [7,11,13,15]
-        for pin in control_pins:
-            GPIO.setup(pin, GPIO.OUT)
-            GPIO.output(pin, 0)
-        halfstep_seq = [
-            [1,0,0,0],
-            [1,1,0,0],
-            [0,1,0,0],
-            [0,1,1,0],
-            [0,0,1,0],
-            [0,0,1,1],
-            [0,0,0,1],
-            [1,0,0,1]
-        ]
-        for i in range(128):
-            for halfstep in range(8):
-                for pin in range(4):
-                    GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
-                time.sleep(0.002)
-        GPIO.cleanup()
-    except Exception as e:
-        print(e)
+    GPIO.setmode(GPIO.BOARD)
+    control_pins = [7,11,13,15]
+    for pin in control_pins:
+        GPIO.setup(pin, GPIO.OUT)
+        GPIO.output(pin, 0)
+    halfstep_seq = [
+        [1,0,0,0],
+        [1,1,0,0],
+        [0,1,0,0],
+        [0,1,1,0],
+        [0,0,1,0],
+        [0,0,1,1],
+        [0,0,0,1],
+        [1,0,0,1]
+    ]
+    for i in range(128):
+        for halfstep in range(8):
+            for pin in range(4):
+                GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
+            time.sleep(0.002)
+    GPIO.cleanup()
 
 
 def get_section():
+    with open('/home/pi/plant-rotator/section_sunlight.txt', 'r') as f:
+        section_value = int(f.readline())
     with open('/home/pi/plant-rotator/current_section.txt', 'r') as f:
-        return int(f.readline())
+        section = int(f.readline())
+
+    if section_value > ROTATE_THRESH:
+        section = (section + 1) % 4
+        with open('/home/pi/plant-rotator/current_section.txt', 'w') as f:
+            f.write('%s\n' % section)
+        section_value = 0
+    return section, section_value
 
 
 def read_metric():
+    import seeed_si114x
     SI1145 = seeed_si114x.grove_si114x()
     return SI1145.ReadVisible
-    # print('Visible %03d UV %.2f IR %03d' % (SI1145.ReadVisible , SI1145.ReadUV/100 , SI1145.ReadIR))
 
 
 def save_metric(metric, value):
@@ -58,23 +62,17 @@ def save_metric(metric, value):
     client.write_points([point])
 
 
-def update_section_value(sun_value, section):
-    with open('/home/pi/plant-rotator/section_sunlight.txt', 'r') as f:
-        starting_value = int(f.readline())
-    new_value = starting_value + max(sun_value - THRESH, 0)
-    if new_value > ROTATE_THRESH:
-        next_section = (section + 1) % 4
-        with open('/home/pi/plant-rotator/current_section.txt', 'w') as f:
-            f.write('%s\n' % next_section)
-        new_value = 0
-        rotate()
+def update_section_value(new_value, section):
     with open('/home/pi/plant-rotator/section_sunlight.txt', 'w') as f:
         f.write('%s\n' % new_value)
 
 
 if __name__  == '__main__':
-    section = get_section()
+    section, section_value = get_section()
     sun_value = read_metric()
+    delta = max(sun_value - THRESH, 0)
+
     save_metric('sunlight-%s' % section, sun_value)
-    save_metric('sunlight-delta-%s' % section, max(sun_value - THRESH, 0))
-    update_section_value(sun_value, section)
+    save_metric('sunlight-delta-%s' % section, delta)
+
+    update_section_value(section_value + delta, section)
