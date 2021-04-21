acc0c36ea30555459c31ef5d82732e87afc51541
Yohei Oka <yoheioka@yoheis-mbp.myfiosgateway.com>
2021-04-21 13:26:50 -0400

    wrape in try except

--- run.py
+++ run.py
@@ -10,28 +10,30 @@ ROTATE_THRESH = 50000
 
 def rotate():
     print('start rotate')
-    GPIO.setmode(GPIO.BOARD)
-    control_pins = [7,11,13,15]
-    for pin in control_pins:
-        GPIO.setup(pin, GPIO.OUT)
-        GPIO.output(pin, 0)
-    halfstep_seq = [
-        [1,0,0,0],
-        [1,1,0,0],
-        [0,1,0,0],
-        [0,1,1,0],
-        [0,0,1,0],
-        [0,0,1,1],
-        [0,0,0,1],
-        [1,0,0,1]
-    ]
-    for i in range(128):
-        for halfstep in range(8):
-            for pin in range(4):
-                GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
-            time.sleep(0.002)
-    GPIO.cleanup()
-    print('end rotate')
+    try:
+        GPIO.setmode(GPIO.BOARD)
+        control_pins = [7,11,13,15]
+        for pin in control_pins:
+            GPIO.setup(pin, GPIO.OUT)
+            GPIO.output(pin, 0)
+        halfstep_seq = [
+            [1,0,0,0],
+            [1,1,0,0],
+            [0,1,0,0],
+            [0,1,1,0],
+            [0,0,1,0],
+            [0,0,1,1],
+            [0,0,0,1],
+            [1,0,0,1]
+        ]
+        for i in range(128):
+            for halfstep in range(8):
+                for pin in range(4):
+                    GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
+                time.sleep(0.002)
+        GPIO.cleanup()
+    except Exception as e:
+        print(e)
 
 
 def get_section():
